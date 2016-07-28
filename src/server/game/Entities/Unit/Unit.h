12c202ad2ef520e16594aa8daab74f05962d75e9
Lukasz Kolasa <maczugapl@gmail.com>
2016-07-28 10:12:55 +0200

    Anticheat system -- we may want to revert this when we'll reach 2-3k stable players, as those checks can be pretty demanding.

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2435,6 +2435,7 @@ class Unit : public WorldObject
     protected:
         explicit Unit (bool isWorldObject);
 
+        GameObject* m_temp_transport;
         void BuildValuesUpdate(uint8 updatetype, ByteBuffer* data, Player* target) const;
 
         UnitAI* i_AI, *i_disabledAI;
